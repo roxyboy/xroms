29d1cdfd2d01af515ba18dd6981d99f6ae98ec67
Uchida Takaya <uchidatakaya@Uchidas-MacBook-Air.local>
2017-07-06 11:11:16 -0400

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,60 +1,29 @@
 import os
-import re
-import sys
-import warnings
-
 from setuptools import setup, find_packages
+PACKAGES = find_packages()
 
-MAJOR = 0
-MINOR = 0
-MICRO = 0
-ISRELEASED = False
-VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
-QUALIFIER = ''
+# Get version and release info, which is all stored in xroms/version.py
+ver_file = os.path.join('xroms', 'version.py')
+with open(ver_file) as f:
+    exec(f.read())
+
+opts = dict(name=NAME,
+            maintainer=MAINTAINER,
+            maintainer_email=MAINTAINER_EMAIL,
+            description=DESCRIPTION,
+            long_description=LONG_DESCRIPTION,
+            url=URL,
+            download_url=DOWNLOAD_URL,
+            license=LICENSE,
+            classifiers=CLASSIFIERS,
+            author=AUTHOR,
+            author_email=AUTHOR_EMAIL,
+            platforms=PLATFORMS,
+            version=VERSION,
+            packages=PACKAGES,
+            package_data=PACKAGE_DATA,
+            requires=REQUIRES)
 
 
-DISTNAME = 'xroms'
-LICENSE = 'MIT'
-AUTHOR = 'Takaya Uchida'
-AUTHOR_EMAIL = 'takaya@ldeo.columbia.edu'
-URL = 'https://github.com/roxyboy/xroms'
-CLASSIFIERS = [
-    'Development Status :: 4 - Beta',
-    'License :: OSI Approved :: Apache Software License',
-    'Operating System :: OS Independent',
-    'Intended Audience :: Science/Research',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.6',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Topic :: Scientific/Engineering',
-]
-
-INSTALL_REQUIRES = ['xarray','dask']
-SETUP_REQUIRES = ['pytest-runner']
-TESTS_REQUIRE = ['pytest >= 2.8', 'coverage']
-
-if sys.version_info[:2] < (2, 7):
-    TESTS_REQUIRE += ["unittest2 == 0.5.1"]
-
-DESCRIPTION = "Utilities for xarray dataarrays/datasets"
-LONG_DESCRIPTION = """To be written.
-"""
-
-setup(name=DISTNAME,
-      version=VERSION,
-      license=LICENSE,
-      author=AUTHOR,
-      author_email=AUTHOR_EMAIL,
-      classifiers=CLASSIFIERS,
-      description=DESCRIPTION,
-      long_description=LONG_DESCRIPTION,
-      install_requires=INSTALL_REQUIRES,
-      setup_requires=SETUP_REQUIRES,
-      tests_require=TESTS_REQUIRE,
-      url=URL,
-      packages=find_packages())
-Contact GitHub API Training Shop Blog About
+if __name__ == '__main__':
+    setup(**opts)
