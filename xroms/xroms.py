586d8c540c5f8357273e33c8a7dc6d835fdfa0e8
Uchida Takaya <uchidatakaya@Uchidas-MacBook-Air.local>
2017-07-08 21:54:40 -0400

    Had the dims name wrong

--- xroms/xroms.py
+++ xroms/xroms.py
@@ -134,6 +134,7 @@ def rel_vorticity(u, v, x, y, dim=None, coord=None):
     vorticity will be on \rho points.
 
     .. math::
+
      \zeta = \frac{\partial v}{\partial x} - \frac{\partial u}{\partial y}
 
     Parameters
@@ -159,7 +160,7 @@ def rel_vorticity(u, v, x, y, dim=None, coord=None):
     if u.dims[-2:] != y.dims or v.dims[-2:] != x.dims:
         raise ValueError("The dimensions of u and y or v and x do not match")
 
-    zeta = ((v.shift(xi_psi=-1) - v) / (x.shift(xi_v=-1) - x)
+    zeta = ((v.shift(xi_pho=-1) - v) / (x.shift(xi_v=-1) - x)
             - (u.shift(eta_rho=-1) - u) / (y.shift(eta_u=-1) - y))
 
     return xr.DataArray(zeta, dims=dim, coords=coord)
