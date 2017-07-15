469349cac50338912da05fdbc0d50987af44884e
Uchida Takaya <uchidatakaya@Uchidas-MacBook-Air.local>
2017-07-15 12:06:50 -0400

    incorporated `xgcm` into the algorithm

--- xroms/xroms.py
+++ xroms/xroms.py
@@ -7,9 +7,13 @@ import scipy.fftpack as fft
 import scipy.integrate as intg
 import scipy.signal as sig
 import dask.array as dsar
+import xgcm.grid as xgd
 
 __all__ = ["sig2z","geo_streamfunc","geo_vel",
            "rel_vorticity","qgpv","pv_inversion"]
+
+def _grid(ds, peri=False):
+    return xgd.Grid(ds, periodic=peri)
 
 def _interpolate(x,y,xnew):
     """
@@ -28,7 +32,7 @@ def sig2z(da, zr, zi, nvar=None):
     ----------
     da : `xarray.DataArray`
         The data on sigma coordinates to be interpolated
-    zr : `xarray.DataArray`
+    zr : `numpy.array`
         The depths corresponding to sigma layers
     zi : `numpy.array`
         The depths which to interpolate the data on
@@ -44,9 +48,9 @@ def sig2z(da, zr, zi, nvar=None):
 
     if np.diff(zi)[0] < 0. or zi.max() <= 0.:
         raise ValueError("The values in `zi` should be postive and increasing.")
-    if zr.ndim > da.ndim:
-        raise ValueError("`da` should have the same "
-                        "or more dimensions than `zr`")
+    if zr.shape != da.shape[-3:]:
+        raise ValueError("`zr` should have the same "
+                        "spatial dimensions as `da`.")
 
     dimd = da.dims
     N = da.shape
@@ -71,12 +75,12 @@ def sig2z(da, zr, zi, nvar=None):
     zi = -zi[::-1] # ROMS has deepest level at index=0
 
     if nvar=='u':  # u variables
-        zl = .5*(zr.shift(eta_rho=-1,xi_rho=-1)
-                 +zr.shift(eta_rho=-1)
+        zl = .5*(np.roll(np.roll(zr, -1, axis=-1), -1, axis=-2)
+                 + np.roll(zr, -1, axis=-2)
                 )
     elif nvar=='v': # v variables
-        zl = .5*(zr.shift(xi_rho=-1)
-                 +zr.shift(eta_rho=-1,xi_rho=-1)
+        zl = .5*(np.roll(zr, -1, axis=-1)
+                 + np.roll(np.roll(zr, -1, axis=-2), -1, axis=-1)
                 )
     else:
         zl = zr
@@ -84,16 +88,16 @@ def sig2z(da, zr, zi, nvar=None):
     for i in range(N[-1]):
         for j in range(N[-2]):
             # only bother for sufficiently deep regions
-            if zl[:,j,i].values.min() < -1e2:
+            if zl[:,j,i].min() < -1e2:
                 # only interp on z above topo
-                ind = np.argwhere(zi >= zl[:,j,i].values.min())
+                ind = np.argwhere(zi >= zl[:,j,i].min())
                 if len(N) == 4:
                     for s in range(N[0]):
-                        dai[s,:len(ind),j,i] = _interpolate(zl[:,j,i].values,
+                        dai[s,:len(ind),j,i] = _interpolate(zl[:,j,i],
                                                             da[s,:,j,i].values,
                                                             zi[int(ind[0]):])
                 else:
-                    dai[:len(ind),j,i] = _interpolate(zl[:,j,i].values,
+                    dai[:len(ind),j,i] = _interpolate(zl[:,j,i],
                                                      da[:,j,i].values,
                                                      zi[int(ind[0]):])
 
@@ -142,7 +146,8 @@ def geo_streamfunc(b, z, f0, inl=0., eta=None, ax=None):
 
     return xr.DataArray(psi, dims=b.dims, coords=b.coords)
 
-def geo_vel(psi, udim=None, ucoord=None, vdim=None, vcoord=None):
+def geo_vel(ds, ds_grid, psi, xname='x_rho', yname='y_rho',
+            peri=False, shift=True):
     """
     Calculates the geostrophic velocity `ug` and `vg`
     on `u` and `v` points respectively
@@ -155,36 +160,56 @@ def geo_vel(psi, udim=None, ucoord=None, vdim=None, vcoord=None):
 
     Parameters
     ----------
-    psi : `xarray.DataArray`
+    ds : `xarray.Dataset`
+        Object that includes all the data necessary
+    ds : `xarray.Dataset`
+        Object that includes the grid information
+    psi : `xarray.Dataset`
         Geostrophic streamfunction
+    shift : boolean (optional)
+        If `True`, the geostrophic velocities will be interpolated
+        to the center of each cell. If `False`, they will be on the
+        same grid points as `u` and `v`.
 
     Returns
     -------
     ug, vg : `xarray.DataArray`
         Zonal and meridional geostrophic velocities.
-        `ug` and `vg` will have different shapes due to the
-        discretized differentiation.
     """
 
-    ug = - ((psi.shift(eta_rho=-1) - psi)
-          / (psi.y.shift(eta_rho=-1) - psi.y)).isel(eta_rho=slice(None,-1))
-    vg = ((psi.shift(xi_rho=-1) - psi)
-        / (psi.x.shift(xi_rho=-1) - psi.x)).isel(xi_rho=slice(None,-1))
-    ug = .25 * (ug + ug.shift(eta_rho=-1) + ug.shift(xi_rho=-1)
-                + ug.shift(eta_rho=-1,xi_rho=-1)
-               ).isel(eta_rho=slice(None,-1),
-                      xi_rho=slice(None,-1)
-                     )
-    vg = .25 * (vg + vg.shift(eta_rho=-1) + vg.shift(xi_rho=-1)
-                + vg.shift(eta_rho=-1,xi_rho=-1)
-               ).isel(eta_rho=slice(None,-1),
-                      xi_rho=slice(None,-1)
-                     )
+    grid = _grid(ds, peri=peri)
+    x = xr.DataArray(ds_grid[xname].values, dims=psi[0,0].dims,
+                     coords=psi[0,0].coords
+                    )
+    y = xr.DataArray(ds_grid[yname].values, dims=psi[0,0].dims,
+                     coords=psi[0,0].coords
+                    )
+    ug = - grid.diff(psi,'Y') / grid.diff(y,'Y')
+    vg = grid.diff(psi,'X') / grid.diff(x,'X')
 
-    return xr.DataArray(ug.values,dims=udim,coords=ucoord), \
-           xr.DataArray(vg.values,dims=vdim,coords=vcoord)
+    if shift:
+        ug = grid.interp(ug, 'Y', boundary='fill')
+        vg = grid.interp(vg, 'X', boundary='fill')
 
-def rel_vorticity(u, v, x, y, dim=None, coord=None, shift=True):
+    # ug = - ((psi.shift(eta_rho=-1) - psi)
+    #       / (psi.y.shift(eta_rho=-1) - psi.y)).isel(eta_rho=slice(None,-1))
+    # vg = ((psi.shift(xi_rho=-1) - psi)
+    #     / (psi.x.shift(xi_rho=-1) - psi.x)).isel(xi_rho=slice(None,-1))
+    # ug = .25 * (ug + ug.shift(eta_rho=-1) + ug.shift(xi_rho=-1)
+    #             + ug.shift(eta_rho=-1,xi_rho=-1)
+    #            ).isel(eta_rho=slice(None,-1),
+    #                   xi_rho=slice(None,-1)
+    #                  )
+    # vg = .25 * (vg + vg.shift(eta_rho=-1) + vg.shift(xi_rho=-1)
+    #             + vg.shift(eta_rho=-1,xi_rho=-1)
+    #            ).isel(eta_rho=slice(None,-1),
+    #                   xi_rho=slice(None,-1)
+    #                  )
+
+    return ug, vg
+
+def rel_vorticity(ds, ds_grid, uname='u', vname='v',
+                  xname='x_v', yname='y_u', shift=True, peri=False):
     """
     Calculates the relative vorticity. ROMS applies a C-grid so the
     vorticity will be on \psi points.
@@ -195,20 +220,14 @@ def rel_vorticity(u, v, x, y, dim=None, coord=None, shift=True):
 
     Parameters
     ----------
-    u : `xarray.DataArray`
-        Zonal velocity
-    v : `xarray.DataArray`
-        Meridional velocity
-    x : `xarray.DataArray`
-        Location along the zonal axis. It should be aligned with `v`.
-    y : `xarray.DataArray`
-        Location along the meridional axis. It should be aligned with `u`.
-    dim : list (optional)
-        Dimensions of `\zeta`
-    coord : dictionary (optional)
-        Coordinates of `\zeta`
+    ds : `xarray.Dataset`
+        Object that includes all the data necessary
+    ds : `xarray.Dataset`
+        Object that includes the grid information
     shift : boolean (optional)
-        Option to shift `\zeta` from `\psi` to `\rho` points
+        If `True`, the geostrophic velocities will be interpolated
+        to the center of each cell. If `False`, they will be on the
+        same grid points as `u` and `v`.
 
     Returns
     -------
@@ -217,26 +236,28 @@ def rel_vorticity(u, v, x, y, dim=None, coord=None, shift=True):
         returned on `\psi` points.
     """
 
-    if u.dims[-2:] != y.dims or v.dims[-2:] != x.dims:
-        raise ValueError("The dimensions of u and y or v and x do not match")
-    if u.shape != v.shape:
-        raise ValueError("`u` and `v` should have the same shape.")
-
-    zeta = (((v.shift(xi_rho=-1) - v)
-             / (x.shift(xi_rho=-1) - x)).isel(eta_v=slice(None,-1),
-                                             xi_rho=slice(None,-1)).values
-            - ((u.shift(eta_rho=-1) - u)
-               / (y.shift(eta_rho=-1) - y)).isel(eta_rho=slice(None,-1),
-                                                xi_u=slice(None,-1)).values
-           )
+    grid = _grid(ds, peri=peri)
+    u = ds[uname][:,::-1]
+    v = ds[vname][:,::-1]
+    x = ds_grid[xname]
+    y = ds_grid[yname]
+    if u.shape[-2:] != y.shape or v.shape[-2:] != x.shape:
+        raise ValueError("The dimensions of u and y and/or "
+                         "v and x do not match")
+    x = xr.DataArray(x.values, dims=v[0,0].dims, coords=v[0,0].coords)
+    y = xr.DataArray(y.values, dims=u[0,0].dims, coords=u[0,0].coords)
+    # if u.shape != v.shape:
+    #     raise ValueError("`u` and `v` should have the same shape.")
+    dvdx = grid.diff(v,'X') / grid.diff(x,'X')
+    dudy = grid.diff(u,'Y') / grid.diff(y,'Y')
+    zeta = dvdx - dudy
 
     if shift:
-        zeta = .25 * np.delete(np.delete((zeta + np.roll(zeta, -1, axis=-1) +
-                      np.roll(zeta, -1, axis=-2)
-                      + np.roll(np.roll(zeta, -1, axis=-2), -1, axis=-1)
-                     ), -1, axis=-1), -1, axis=-2)
+        zeta = grid.interp(grid.interp(zeta, 'X', boundary='extend'),
+                           'Y', boundary='extend'
+                          )
 
-    return xr.DataArray(zeta, dims=dim, coords=coord)
+    return zeta
 
 def _interp_vgrid(nz,ny,nx,z,H):
     """Generate grid for vertical finite differences"""
